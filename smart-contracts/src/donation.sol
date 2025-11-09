d294bdbdbf5e05194ebb0b26e148c0cfb98dd575
godinandi <50024725+godinandi@users.noreply.github.com>
2025-11-09 17:27:15 +0700

    tespush

--- smart-contracts/src/donation.sol
+++ smart-contracts/src/donation.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
 contract Donation {
     // state variables
     address public admin;
