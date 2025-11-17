8696234502675083249e56af72c63703410bf393
paul <email@email.com>
2025-11-17 11:36:12 +0700

    init FE code

---
+++ frontend/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import WalletButton from "./components/WalletButton";
+
+export default function Page() {
+  return (
+    <>
+      <WalletButton />
+      <main style={{ padding: 40 }}>
+        <h1>Donation Solution</h1>
+        <p>Donate with blockchain.</p>
+      </main>
+    </>
+  );
+}
