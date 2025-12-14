8e9b4f1ff9bfaeb1e5c49cc75bb2c9d06a1e9364
godinandi <50024725+godinandi@users.noreply.github.com>
2025-12-14 20:09:55 +0700

    naikkan tinggi page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
    <body className="min-h-screen bg-gray-50 antialiased">
       <Header />
 
-      <main className="pt-12 md:pt-14">
+      <main className="pt-4">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {children}
         </div>
